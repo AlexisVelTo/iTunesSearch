4569644d2795f85e4b4755cb7195e6967dbf689f
unknown <alexisvelto@gmail.com>
2019-04-29 14:36:56 -0400

    First commit

---
+++ app/src/main/java/cl/alexisvelto/itunessearch/MainActivity.kt
@@ -0,0 +1,12 @@
+package cl.alexisvelto.itunessearch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
